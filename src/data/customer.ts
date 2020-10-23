5c6978caf00c16043dcc47cae2680a246b154375
wawanriyanto9 <wawanriyanto9@gmail.com>
2020-10-24 06:40:18 +0700

    commit

---
+++ src/data/customer.ts
@@ -0,0 +1,70 @@
+import { CustomerType } from "../mongoose";
+
+const customers: CustomerType[] = [
+    { 
+    first_name: "Michael", 
+    last_name: "Chandra", 
+    age: 29, 
+    customer_type: "employee", 
+    street: "1224 Origins", 
+    city: "Selangor", 
+    state: "Malaysia", 
+    zip_code: "45417",
+    phone_number: "(346)-135-2467" 
+}, 
+    { 
+        first_name: "Adam", 
+        last_name: "Malik", 
+        age: 32, 
+        customer_type: "employee", 
+        street: "1246 Meliana Street", 
+        city: "Spokane", 
+        state: "Indiana", 
+        zip_code: "65626",
+        phone_number: "(183)-068-1703" 
+    }, 
+    { 
+        first_name: "Marck", 
+        last_name: "Harieer", 
+        age: 30, 
+        customer_type: "student", 
+        street: "8732 Robinson Rd", 
+        city: "Orange", 
+        state: "Pennsylvania", 
+        zip_code: "31168",
+        phone_number: "(377)-762-7780" 
+    }, 
+    { 
+        first_name: "Chlarra", 
+        last_name: "Aer", 
+        age: 38, 
+        customer_type: "employee", 
+        street: "2199 Preston Rd", 
+        city: "Columbia", 
+        state: "Arkansas", 
+        zip_code: "97400",
+        phone_number: "(050)-371-8203" 
+    }, 
+    { 
+        first_name: "Nircholas", 
+        last_name: "Zark", 
+        age: 19, 
+        customer_type: "student", 
+        street: "335 Rolling Green Rd", 
+        city: "Wichita Falls", 
+        state: "Kansas", 
+        zip_code: "95191", 
+        phone_number: "(339)-055-3014" }, 
+    { 
+        first_name: "Samuel", 
+        last_name: "Yudistira", 
+        age: 36, 
+        customer_type: "employee", 
+        street: "1738 Blossom Hill Rd", 
+        city: "Addiss", 
+        state: "Oregon", 
+        zip_code: "61178",
+        phone_number: "(482)-912-3722" },
+]
+
+export default customers
